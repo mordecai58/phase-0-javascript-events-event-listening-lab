5cf169ca5cc42139591235ffe6a0e2d0c8d3c40a
mordecai58 <mordecaijakopiyo8@gmail.com>
2024-04-01 00:27:28 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input = document.getElementById('button').addingEventListener('click', function () {
+        alert('i have been clicked');
+    });
 }
+addingEventListener();
